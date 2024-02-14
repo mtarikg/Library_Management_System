52d50f3d1642cc1f7f3894c8dd2e24fcc83892a8
Tarik Gol <mtarikgol@gmail.com>
2024-02-14 13:31:25 +0300

    Book listing implemented

--- library.py
+++ library.py
@@ -1,3 +1,22 @@
 # TO-DO list:
-# 1) Create a Library class
-# 2) Add three functionalities: Listing, Adding and Removing (a) book(s)
+# 1) Create a Library class ✔
+# 2) Add the following functionalities:
+#       Listing ✔,
+#       Adding a book,
+#       Removing a book
+
+class Library:
+    def __init__(self, file_name):
+        self.file = open(file_name, "a+")
+
+    def __del__(self):
+        self.file.close()
+
+    def list_books(self):
+        self.file.seek(0)
+        books = self.file.read().splitlines()
+        for book in books:
+            data = book.split(",")
+            title = data[0]
+            author = data[1]
+            print(f"Title: {title} Author: {author}")
